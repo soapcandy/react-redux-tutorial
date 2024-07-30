edf97ce7d86fcbc65c5000a90af62cbc36cc1c00
박지훈 <pjh@ziumks.com>
2024-07-30 14:49:35 +0900

    애플리케이션에 리덕스 적용하기

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./modules/index.jsx";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>
+);
