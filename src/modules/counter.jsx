1c56c7a8fe7192c13ea2bcc0f02594c3957d5340
박지훈 <pjh@ziumks.com>
2024-07-30 14:36:29 +0900

    리덕스 관련 코드 작성하기

---
+++ src/modules/counter.jsx
@@ -0,0 +1,29 @@
+// actions
+const INCREASE = "counter/INCREASE";
+const DECREASE = "counter/DECREASE";
+
+// constants
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
+
+// reducers
+const initialState = {
+  number: 0,
+};
+
+function counter(state = initialState, action) {
+  switch (action.type) {
+    case INCREASE:
+      return {
+        number: state.number + 1,
+      };
+    case DECREASE:
+      return {
+        number: state.number - 1,
+      };
+    default:
+      return state;
+  }
+}
+
+export default counter;
