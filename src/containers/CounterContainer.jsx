a91c8563704578f39c2fc235bd06ca12c34b3be9
박지훈 <pjh@ziumks.com>
2024-07-30 16:14:38 +0900

    컨테이너 컴포넌트 만들기

---
+++ src/containers/CounterContainer.jsx
@@ -0,0 +1,57 @@
+import { connect } from "react-redux";
+import Counter from "../components/Counter";
+import { decrease, increase } from "../modules/counter";
+
+const CounterContainer = ({ number, increase, decrease }) => {
+  return (
+    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+  );
+};
+
+// 일반적인 방식
+// const mapStateToProps = (state) => ({
+//   number: state.counter.number,
+// });
+// const mapDispatchToProps = (dispatch) => ({
+//   increase: () => {
+//     dispatch(increase());
+//   },
+//   decrease: () => {
+//     dispatch(decrease());
+//   },
+// });
+// export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+
+// 함수형 방식
+// export default connect(
+//   (state) => ({
+//     number: state.counter.number,
+//   }),
+//   (dispatch) => ({
+//     increase: () => dispatch(increase()),
+//     decrease: () => dispatch(decrease()),
+//   })
+// )(CounterContainer);
+
+// // 유틸 함수형 방식
+// export default connect(
+//   (state) => ({
+//     number: state.counter.number,
+//   }),
+//   (dispatch) =>
+//     bindActionCreators(
+//       {
+//         increase,
+//         decrease,
+//       },
+//       dispatch
+//     )
+// )(CounterContainer);
+
+// 액션 함수형 방식(mapDispatchToProps파라미터를 사용)
+export default connect(
+  (state) => ({
+    number: state.counter.number,
+  }),
+  { increase, decrease }
+)(CounterContainer);
